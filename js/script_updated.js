a6d0de1efed843b9a663384c761583d13287f984
Muhammad Reza Pahrevi <rezapahrevi2354@gmail.com>
2024-12-05 20:43:15 +0700

    Update 20;43

--- js/script_updated.js
+++ js/script_updated.js
@@ -1,15 +1,15 @@
-function gethasilperhitungan(){
-  const umur = document.getElementById("umur"). value;
-  const tinggibadan = document.getElementById("tinggibadan"). value;
-  const beratbadan = document.getElementById("beratbdan"). value;
+// function validasi nya masih error
+function gethasilperhitungan() {
+  const umur = document.getElementById("umur").value;
+  const tinggibadan = document.getElementById("tinggibadan").value;
+  const beratbadan = document.getElementById("beratbdan").value;
 
-  const tinggibadan = '200';
-  const beratbadan = '150';
+  const tinggibadan = "200";
+  const beratbadan = "150";
 
-  if (umur == '' || tinggibadan == '' || beratbadan == ''){
+  if (umur == "" || tinggibadan == "" || beratbadan == "") {
     alert("Data tidak boleh ada yang kosong");
   }
 }
 
 // Maaf sebelumnya, untuk hasil miniproject saya code javascript nya belum bisa saya selesaikan, bukan nya saya tdk mencari referensi dll, pemahaman saya terhadap code js nya belum mengerti sama sekali, saya sdh coba selama miniprojeck ini berjalan, maka dari itu saya hanya bisa menyelesaikan sampai #css nya saja.
-
